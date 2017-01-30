4b73f43ef7a4558aabd555dede1c356452b93f0e
Thomas Kioko <kiokotomas@gmail.com>
2017-01-30 14:41:15 +0300

    Implement lambdas

--- app/src/main/java/com/thomaskioko/lambdademo/MainActivity.java
+++ app/src/main/java/com/thomaskioko/lambdademo/MainActivity.java
@@ -5,27 +5,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fab)
+    FloatingActionButton mFloatingActionButton;
+    @BindView(R.id.btnClick)
+    Button mButtonClick;
+    @BindView(R.id.toolbar)
+    Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolBar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        mFloatingActionButton.setOnClickListener(view -> Snackbar.make(view, "Lambdas Rock", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        //Method reference
+        mButtonClick.setOnClickListener(this::showSnackBar);
     }
 
     @Override
@@ -48,5 +57,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Helper method used to display a snackbar.
+     *
+     * @param view {@link View}
+     */
+    private void showSnackBar(View view) {
+        Snackbar.make(view, "Method Reference", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
