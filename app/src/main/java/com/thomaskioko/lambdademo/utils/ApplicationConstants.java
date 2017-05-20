38ea75c887eaf6191da859e166d8bc0a701a01f0
Thomas Kioko <kiokotomas@gmail.com>
2017-05-20 15:02:47 +0300

    create app constant class

---
+++ app/src/main/java/com/thomaskioko/lambdademo/utils/ApplicationConstants.java
@@ -0,0 +1,26 @@
+package com.thomaskioko.lambdademo.utils;
+
+/**
+ * Class contains variables used across the application.
+ *
+ * @author Thomas Kioko
+ */
+
+public class ApplicationConstants {
+    /**
+     * Connection timeout duration
+     */
+    public static final int CONNECT_TIMEOUT = 60 * 1000;
+    /**
+     * Connection Read timeout duration
+     */
+    public static final int READ_TIMEOUT = 60 * 1000;
+    /**
+     * Connection write timeout duration
+     */
+    public static final int WRITE_TIMEOUT = 60 * 1000;
+    /**
+     * Endpoint
+     */
+    public static final String BASE_URL = "http://api.theysaidso.com";
+}
