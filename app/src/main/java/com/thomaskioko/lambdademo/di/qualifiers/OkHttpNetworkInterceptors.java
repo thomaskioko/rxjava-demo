0891ee624949e9b81550ed92dc2d703946838333
Thomas Kioko <kiokotomas@gmail.com>
2017-05-23 12:26:29 +0300

    create okhttp interceptors

---
+++ app/src/main/java/com/thomaskioko/lambdademo/di/qualifiers/OkHttpNetworkInterceptors.java
@@ -0,0 +1,18 @@
+package com.thomaskioko.lambdademo.di.qualifiers;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author Thomas Kioko
+ */
+
+@Documented
+@Qualifier
+@Retention(RUNTIME)
+public @interface OkHttpNetworkInterceptors {
+}
